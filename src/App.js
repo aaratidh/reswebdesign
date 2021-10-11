ef29f1b9fcb8a30378e9ac2e5c4b027a33dd874a
MagicianAD <amazingaarati@gmail.com>
2021-10-12 01:17:57 +0545

    Pricing is added laong with footer Data are also updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Homepage from "./Components/Pages/HomePages/Homepage.jsx";
+import Footer from "./Components/Pages/Footer/Footer";
 function App() {
   return (
     <Router>
@@ -9,6 +10,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={Homepage} />
       </Switch>
+      <Footer />
     </Router>
   );
 }
--- src/Components/Pages/HomePages/Homepage.jsx
+++ src/Components/Pages/HomePages/Homepage.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Homehero from "../../Pages/Homehero/Homehero.jsx";
-import { homeObjone } from "./Data.jsx";
+import { homeObjone, homeObjTwo, homeObjThree, homeObjFour } from "./Data.jsx";
+import Pricing from "../../Pricing/Pricing.jsx";
 function Data() {
   return (
     <div>
       <Homehero {...homeObjone} />
+      <Homehero {...homeObjTwo} />
+      <Homehero {...homeObjThree} />
+      <Pricing />
+      <Homehero {...homeObjFour} />
     </div>
   );
 }
